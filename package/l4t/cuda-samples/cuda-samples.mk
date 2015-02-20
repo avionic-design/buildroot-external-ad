82b663daf6cf5b1b99c556af6882e6e0029ccefc
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2015-02-20 16:34:07 +0100

    Add precompiled CUDA sample binaries
    Change-Id: Ife43f4147b2e82f49aa6b8bb302f13fe9bdc5aa2
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/2924
    Reviewed-by: Julian Scheel <julian@jusst.de>

---
+++ package/l4t/cuda-samples/cuda-samples.mk
@@ -0,0 +1,18 @@
+CUDA_SAMPLES_VERSION = 6.5
+CUDA_SAMPLES_SITE = http://developer.download.nvidia.com/embedded/jetson/TK1/2014-03-24
+CUDA_SAMPLES_SOURCE = CUDA_SDK_Samples.tgz
+CUDA_SAMPLES_LICENSE = EULA
+CUDA_SAMPLES_REDISTRIBUTE = NO
+CUDA_SAMPLES_DEPENDENCIES = cuda
+
+dash-version := $(subst .,-,$(CUDA_SAMPLES_VERSION))
+
+cuda-samples-bindir = /usr/local/cuda-$(CUDA_SAMPLES_VERSION)/samples/bin
+
+define CUDA_SAMPLES_INSTALL_TARGET_CMDS
+	$(INSTALL) -d '$(TARGET_DIR)$(cuda-samples-bindir)'
+	$(TAR) -C '$(@D)' -cO . | \
+		$(TAR) -C '$(TARGET_DIR)$(cuda-samples-bindir)' $(TAR_OPTIONS) -
+endef
+
+$(eval $(generic-package))
