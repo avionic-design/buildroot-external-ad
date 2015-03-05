b126182a334682ff3f5cd7b06ab7899b0690c2b6
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2015-03-05 12:43:40 +0100

    board: meerkat-l4t: simplify post-install script
    Replace two lengthy sed commands with a more readable short function.
    
    Change-Id: I0c5c0d8608560158b65734d4dc72223dc70f500e
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/3106
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- board/meerkat-l4t/post.sh
+++ board/meerkat-l4t/post.sh
@@ -15,9 +15,11 @@ echo "Build uImage files for device-trees: ${DTS}"
 # export $BR2_EXTERNAL to post-build scripts
 BR2_EXTERNAL=${BR2_EXTERNAL:-../buildroot-external-ad/}
 
-sed "s/IMAGES/$(echo ${IMAGES} | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/g" ${BR2_EXTERNAL}/board/meerkat-l4t/kernel_fdt.its.in > /tmp/kernel_fdt.its.in
+escape() { echo "$*" | sed 's,[\/&],\\&,g'; }
+
+sed "s/IMAGES/$(escape "${IMAGES}")/g" ${BR2_EXTERNAL}/board/meerkat-l4t/kernel_fdt.its.in > /tmp/kernel_fdt.its.in
 for DT in ${DTS}; do
 	echo "Building uImage with FDT ${DT} in ${IMAGES}"
-	sed "s/DEVICETREE/$(echo ${DT} | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/g" /tmp/kernel_fdt.its.in > /tmp/kernel_fdt.its
+	sed "s/DEVICETREE/$(escape "${DT}")/g" /tmp/kernel_fdt.its.in > /tmp/kernel_fdt.its
 	mkimage -f /tmp/kernel_fdt.its ${IMAGES}/uImage-${DT}
 done
