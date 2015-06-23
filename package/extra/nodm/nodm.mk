e1f245008411d9e9a18b39d736961d9ea014c051
Julian Scheel <julian@jusst.de>
2015-06-23 10:40:55 +0200

    nodm: Add new package
    Change-Id: Ieadb43a9e650e6531bf1e24d8f0d9d911a5b0b2a
    Signed-off-by: Julian Scheel <julian@jusst.de>
    Reviewed-on: http://review.adnet.avionic-design.de/3655
    Reviewed-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>

---
+++ package/extra/nodm/nodm.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# nodm
+#
+################################################################################
+
+NODM_VERSION = 0.7
+NODM_SITE = http://www.enricozini.org/sw/nodm/
+NODM_LICENSE = GPLv2
+NODM_LICENSE_FILES = COPYING
+NODM_DEPENDENCIES = linux-pam
+NODM_INSTALL_STAGING = NO
+
+define NODM_INSTALL_EXTRA
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL)/package/extra/nodm/S90nodm \
+		$(TARGET_DIR)/etc/init.d/S90nodm
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL)/package/extra/nodm/pam.nodm \
+		$(TARGET_DIR)/etc/pam.d/nodm
+endef
+
+NODM_POST_INSTALL_TARGET_HOOKS += NODM_INSTALL_EXTRA
+
+$(eval $(autotools-package))
