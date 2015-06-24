4adba6f449bad93aa3ae4ff4de32126c92fe6e2b
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2015-06-24 21:03:48 +0200

    Add post-build script scrubbing files that leak from staging or host to the target
    Some upstream buildroot packages are buggy and install files into
    $TARGET_DIR/$STAGING_DIR, or likewise $TARGET_DIR/$HOST_DIR.  Since we
    do not (yet) fork buildroot itself, we cannot fix these buildroot
    packages.  As a workaround, add a post-build script cleaning up that
    mess.
    
    Change-Id: Iaa54999ae8c2dd43e36676c1ac1b253f95c84578
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/3667
    Reviewed-by: Julian Scheel <julian@jusst.de>

---
+++ board/meerkat-l4t/post-build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+cd "$TARGET_DIR"
+
+if test -d "./$STAGING_DIR"; then
+	echo "Scrubbing bogus files from target that leaked from staging:"
+	rm -rv "./$STAGING_DIR"
+fi
+if test -d "./$HOST_DIR"; then
+	echo "Scrubbing bogus files from target that leaked from host:"
+	rm -rv "./$HOST_DIR"
+fi
+if test -d "./$BASE_DIR"; then
+	echo "Removing leftover directory tree ./$BASE_DIR"
+	rmdir -p --ignore-fail-on-non-empty "./$BASE_DIR"
+fi
