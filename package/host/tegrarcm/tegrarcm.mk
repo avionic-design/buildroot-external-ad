e368cc69cec78e419be0e7353bb46cfb32dec7c1
Julian Scheel <julian@jusst.de>
2016-07-27 17:15:18 +0200

    package/tegrarcm: Update to git snapshot
    We need a recent git snapshot of tegrarcm, as our dfu-flasher script makes use
    of the newly added usb-timeout cmdline option. Unfortunately no release has
    been made of tegrarcm in the meantime, so we have to use a snapshot for now.
    
    Change-Id: I45ecf65c40230f9aecc0fb77e532e67c30f5dea7
    Signed-off-by: Julian Scheel <julian@jusst.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6149
    Reviewed-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>

--- package/host/tegrarcm/tegrarcm.mk
+++ package/host/tegrarcm/tegrarcm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TEGRARCM_VERSION = v1.7
+TEGRARCM_VERSION = ec1eeac
 TEGRARCM_SITE = $(call github,NVIDIA,tegrarcm,$(TEGRARCM_VERSION))
 TEGRARCM_LICENSE = BSD-3c / NVIDIA Software License (src/miniloader)
 TEGRARCM_LICENSE_FILE = LICENSE
