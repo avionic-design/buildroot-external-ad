bbad0b89372b5b930853a671fda6dd5cd0d02efb
Alban Bedel <alban.bedel@avionic-design.de>
2016-08-09 17:46:15 +0200

    enlightenment: Add an optional dependency on udisks
    Enlightenment needs udisks for auto mounting support, we add it as
    optional dependency to have udisks built before enlightenment when it
    is selected.
    
    Change-Id: Ia7beafd259bae11ba9bc7c8e85c1c7268222bf4c
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6264
    Reviewed-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>

--- external.mk
+++ external.mk
@@ -1,1 +1,6 @@
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*/*.mk))
+
+# Make sure enlightenment is built with udisks support
+ifeq ($(BR2_PACKAGE_UDISKS),y)
+ENLIGHTENMENT_DEPENDENCIES += udisks
+endif
