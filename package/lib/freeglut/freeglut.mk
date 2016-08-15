feb7f29a1b2ac51644a222949f879ccda0f63679
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2016-08-15 15:27:45 +0200

    freeglut: Replace freeglut v2.8 with upstream package v3.0
    freeglut has meanwhile been packaged by buildroot upstream; sync our
    freeglut package with theirs.  This updates freeglut from v2.8 to v3.0
    and renames the package from freeglut to libfreeglut.
    
    Change-Id: I76ea6c8022bcf191c59332d9f4bb4a0925948662
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6391
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- package/lib/freeglut/freeglut.mk
+++
@@ -1,9 +0,0 @@
-FREEGLUT_VERSION = 2.8.1
-FREEGLUT_SOURCE = freeglut-$(FREEGLUT_VERSION).tar.gz
-FREEGLUT_SITE = http://downloads.sourceforge.net/project/freeglut/freeglut/$(FREEGLUT_VERSION)
-FREEGLUT_LICENSE = MIT
-FREEGLUT_INSTALL_STAGING = YES
-FREEGLUT_INSTALL_TARGET = YES
-FREEGLUT_DEPENDENCIES = libgl libglu
-
-$(eval $(autotools-package))
