eaa01178aaa8ed0f3465aa987c7d370b003f0b0b
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2016-08-31 18:42:13 +0200

    post-build: Add some structuring comments
    The post-build script really has been accumulating multiple distinct
    things, so add some comments that structure the script a bit.
    
    Change-Id: Iba52d3685750251ce171d5e54127aa966fce7ecb
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6511
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- board/meerkat-l4t/post-build.sh
+++ board/meerkat-l4t/post-build.sh
@@ -4,6 +4,10 @@ set -e
 
 cd "$TARGET_DIR"
 
+#
+# Scrub files from the target that can be identified as having leaked
+# from staging or host directories.
+#
 if test -d "./$STAGING_DIR"; then
 	echo "Scrubbing bogus files from target that leaked from staging:"
 	rm -rv "./$STAGING_DIR"
@@ -17,6 +21,9 @@ if test -d "./$BASE_DIR"; then
 	rmdir -p --ignore-fail-on-non-empty "$(readlink -f ./"$BASE_DIR")"
 fi
 
+#
+# Enable ssh login with password
+#
 if [ -e etc/ssh/sshd_config ]
 then
 	echo "Enabling ssh root login with password."
@@ -25,6 +32,9 @@ PermitRootLogin yes
 ' etc/ssh/sshd_config
 fi
 
+#
+# Add debugfs to /etc/fstab
+#
 if ! awk '$1 !~ /^#/ && $2 == "/sys/kernel/debug"' etc/fstab | grep -q .
 then
 	printf "debugfs\t\t/sys/kernel/debug\tdebugfs\tdefaults\t0\t0\n" \
