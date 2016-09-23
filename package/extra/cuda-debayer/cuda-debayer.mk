f20200f36e518d8548e67a85e445a194ba652030
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2016-09-23 10:38:48 +0200

    cuda-debayer: Make graphics backend configurable
    Let the user choose if the support for rendering with opencv and GL
    should be built.
    
    Change-Id: I36eef4765152840d2f2a0ec34d4aa8a445ec56b7
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6659
    Reviewed-by: Meike Vocke <meike.vocke@avionic-design.de>

--- package/extra/cuda-debayer/cuda-debayer.mk
+++ package/extra/cuda-debayer/cuda-debayer.mk
@@ -4,12 +4,23 @@ CUDA_DEBAYER_LICENSE = GPLv2
 CUDA_DEBAYER_AUTORECONF = YES
 
 CUDA_DEBAYER_DEPENDENCIES += host-automake host-autoconf host-libtool \
-			     host-cuda-cross-toolchain \
-			     cuda libgl libglu libglew libfreeglut
+			     host-cuda-cross-toolchain cuda
+
+ifeq ($(BR2_PACKAGE_CUDA_DEBAYER_GL),y)
+CUDA_DEBAYER_DEPENDENCIES += libgl libglu libglew libfreeglut
+CUDA_DEBAYER_CONF_OPTS += --with-opengl
+else
+CUDA_DEBAYER_CONF_OPTS += --without-opengl
+endif
+
+ifeq ($(BR2_PACKAGE_CUDA_DEBAYER_OPENCV),y)
+CUDA_DEBAYER_DEPENDENCIES += opencv4tegra
+CUDA_DEBAYER_CONF_OPTS += --with-opencv
+else
+CUDA_DEBAYER_CONF_OPTS += --without-opencv
+endif
 
 CUDA_DEBAYER_CONF_ENV += NVCC=$(HOST_DIR)/usr/local/cuda/bin/nvcc \
 			CXXFLAGS='$(TARGET_CXXFLAGS) -O2 -g'
-
-CUDA_DEBAYER_CONF_OPTS += --with-opengl --without-opencv
 
 $(eval $(autotools-package))
